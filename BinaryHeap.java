f528c203c84842ca262a4b46f8cd27f41c0414e5
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-11-02 15:08:36 -0700

    Cleaned up file and added javadoc to code

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -3,11 +3,19 @@ public class BinaryHeap {
 	private int[] arr;
 	private int size;
 	
+	/**
+	 * Initializes the Heap / Priority Queue
+	 */
 	public BinaryHeap() {
 		this.arr = new int[10];
 		this.size = 0;
 	}
 
+	/**
+	 * Adds a number to the Heap and puts it in the correct place
+	 *
+	 * @param num The number to add to the heap
+	 */
 	public void add(int num) {
 		if (size >= this.arr.length) {
 			growArray();
@@ -26,10 +34,14 @@ public class BinaryHeap {
 
 	}
 
-	public int remove() /*throws Exception*/ {
+	/**
+	 * Removes the highest priority num from the heap
+	 * 
+	 * @return int The highest priority num(lowest num)
+	 */
+	public int remove() {
 		if (isEmpty()) {
 			return -1;
-			//throw new Exception("Heap is empty");
 		}
 		int lowest = this.arr[0];
 		this.arr[0] = this.arr[this.size - 1];
@@ -38,6 +50,9 @@ public class BinaryHeap {
 		return lowest;
 	}
 
+	/**
+	 * Doubles the size of the current arr 
+	 */
 	public void growArray() {
 		int[] newArr = new int[this.arr.length * 2];
 		for (int i = 0; i < size; i++) {
@@ -46,6 +61,11 @@ public class BinaryHeap {
 		this.arr = newArr;
 	}
 
+	/**
+	 * Shifts an element down the heap to where it belongs 
+	 *
+	 * @param parent The parent of the element to check for swap
+	 */
 	public void shiftDown(int parent) {
 		int child = parent * 2 + 1;
 		if (child > size) {
@@ -60,12 +80,22 @@ public class BinaryHeap {
 		}
 	}
 
-	static void swap(int[] a, int index1, int index2) {
+	/**
+	 * Swaps two elements in an arr given two indexes
+	 *
+	 * @param a The arr you want to swap indexes in
+	 * @param index1 Index of the first element you want to swap
+	 * @param index2 Index of second element you want to swap
+	 */
+	public static void swap(int[] a, int index1, int index2) {
 		int temp = a[index1];
 		a[index1] = a[index2];
 		a[index2] = temp;
 	}
 
+	/**
+	 * Checks if the Heap is empty or not
+	 */
 	public boolean isEmpty() {
 		if (this.size <= 0) {
 			return true;
